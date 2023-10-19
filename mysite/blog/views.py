a2121160068fa747419e0030160ebdf29aacc9bd
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-10-19 20:50:41 +0300

    Add to views.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Post
 
 # Create your views here.
@@ -9,3 +10,14 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'post': posts})
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No Post found.")
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post})
