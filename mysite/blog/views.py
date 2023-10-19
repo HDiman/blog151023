9df080f39fe83fa23009ea34eb218496ed773412
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-10-19 21:00:27 +0300

    Add2 to views.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 
@@ -13,11 +13,15 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+    post = get_object_or_404(Post,
+                             id=id,
+                             status=Post.status.PUBLISHED)
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found.")
 
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
